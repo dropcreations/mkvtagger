6118500b3a2452703a00a1dc42a3dd6e80d72902
drop_codes <97192664+dropcreations@users.noreply.github.com>
2022-08-26 22:53:47 +0530

    setup.py added!

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'mkvtagger',
+    version = '0.0.0',
+    author = 'dropcreations',
+    author_email = 'dinitharwijesinghe@gmail.com',
+    url = 'https://github.com/dropcreations/MKV_Tagger',
+    license = 'MIT',
+    description = 'Tag MKV/WebM files',
+    long_description = open('README.md', 'r').read(),
+    packages = find_packages(),
+    py_modules = ['xmlformatter'],
+    entry_points = {
+        'console_scripts': ['mkvtagger=mkvtagger:main'],
+    }
+)
