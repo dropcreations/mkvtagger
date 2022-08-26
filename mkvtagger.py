2537e7bf08e9920d411d43a59c4adffde34903c9
drop_codes <97192664+dropcreations@users.noreply.github.com>
2022-08-26 22:53:01 +0530

    Updated!

--- mkvtagger.py
+++ mkvtagger.py
@@ -3,106 +3,138 @@ import sys
 import subprocess
 from xml.etree.ElementTree import Element, tostring
 
-file_path = sys.argv[1]
-file_name = os.path.basename(os.path.abspath(file_path))
-file_name = os.path.splitext(file_name)
-file_name = file_name[0]
-xml_name = file_name + "_tags.xml"
-tags_dict = {}
+tagsDict = {}
+inputList = []
+inputCount = len(sys.argv)
 
-print("Matroska/WebM Tags")
-print("|")
-title_name = input("|--Title: ")
-print("|")
-print("|--Add Tags from a Text file")
-tags_text_add = input("|--Type 'Yes' or 'No': ")
-if tags_text_add.lower() == "yes":
-    print("|")
-    print("|--Text file's content format must be as below.")
-    print("|-----------------------------------------")
-    print("|-- Tag_Name_1: Tag_Value_1, Tag_Value_2")
-    print("|-- Tag_Name_2: Tag_Value_1, Tag_Value_2")
-    print("|-----------------------------------------")
-    tags_text = input("|--Enter Text file's Path: ")
-    print("|")
-    txt_folder = os.path.dirname(os.path.abspath(tags_text))
-    txt_file = os.path.basename(os.path.abspath(tags_text))
-    os.chdir(txt_folder)
-    with open(txt_file) as txt_tags:
-        for custom_tags in txt_tags:
-            custom_tags = custom_tags.strip()
-            print("|--" + custom_tags)
-            tag_name_add = custom_tags.split(': ')
-            contributor = tag_name_add[-1].split(', ')
-            tag_values = ' / '.join(contributor)
-            tags_dict[tag_name_add[0]] = tag_values
-else:
-    print("|")
-    print("|--Enter Tag Name first and Enter Tag Value second.")
-    print("|--When finished, Type 'done' in Tag Name.")
+# Collects tag names and tag values to a dictionary
 
-    while True:
-        enter_tag_name = input("|--Enter Tag Name: ")
-        if enter_tag_name.lower() == "done":
-            break
-        else:
-            tag_value = input("|--" + enter_tag_name + ": ")
-            contributor = tag_value.split(', ')
-            tag_values = ' / '.join(contributor)
-            tags_dict[enter_tag_name] = tag_values
+def get_tags(inputFile):
+    
+    """ Collecting tag data """
 
-tags_node = Element('Tags')
-tag_node = Element('Tag')
-for key, values in tags_dict.items():
-    simple_node = Element('Simple')
-    name_node = Element('Name')
-    name_node.text = key
-    simple_node.append(name_node)
-    string_node = Element('String')
-    string_node.text = values
-    simple_node.append(string_node)
-    tag_node.append(simple_node)
-
-tags_node.append(tag_node)
-xml_data = tostring(tags_node)
-
-def execute_propedit():
-    cmd_line = '"' + os.path.abspath(file_path) + '" --tags all:' + xml_name + ' --edit info '
-    if title_name.lower() == "delete":
-        cmd_line = cmd_line + "--delete title "
+    print(
+        f"\nMatroska/WebM Tags : {os.path.basename(inputFile)}\
+        \n|"
+    )
+    global Title; Title = input("|--Title: ")
+    tagsText = input(
+        "|\
+        \n|--Do you want to add tags from a text document? [y/n] "
+    )
+    if (tagsText.lower() == "y") or (tagsText.lower() == "yes"):
+        tagsTextPath = input("|--Text document's path: ")
+        if tagsTextPath.startswith('"') and tagsTextPath.endswith('"'):
+                tagsTextPath = tagsTextPath[1:-1]
+        print("|")
+        textFolder = os.path.dirname(tagsTextPath)
+        textFile = os.path.basename(tagsTextPath)
+        os.chdir(textFolder)
+        with open(textFile) as txtDoc:
+            for customTag in txtDoc:
+                customTag = customTag.strip()
+                print("|--" + customTag)
+                tagName = customTag.split(': ')
+                tagValue = tagName[1].split(', ')
+                tagValue = ', '.join(tagValue)
+                tagsDict[tagName[0]] = tagValue
+    elif (tagsText.lower() == "n") or (tagsText.lower() == "no"):
+        print("|")
+        print("|--Type [tag name] first and [tag value] second.")
+        print("|--When finished, Type 'done' in [tag name].")
+        print("|")
+        while True:
+            tagName = input("|--Tag name: ")
+            if tagName.lower() == "done":
+                break
+            else:
+                tagValue = input(f'|--{tagName}: ')
+                tagValue = tagValue.split(', ')
+                tagValue = ', '.join(tagValue)
+                tagsDict[tagName] = tagValue
     else:
-        cmd_line = cmd_line + '--set title="' + title_name + '" '
-    print("|")
-    print("|--Do you want to remove 'Encoded date'?")
-    delete_encoded_date = input("|--Type 'Yes' or 'No': ")
-    print("|")
-    print("|--Do you want to remove 'Writing application'?")
-    delete_writing_application = input("|--Type 'Yes' or 'No': ")
-    print("|")
-    print("|--Do you want to remove 'Writing library'?")
-    delete_writing_library = input("|--Type 'Yes' or 'No': ")
-    if delete_encoded_date.lower() == "yes":
-        cmd_line = cmd_line + "--delete date "
-    if delete_writing_application.lower() == "yes":
-        cmd_line = cmd_line + '--set writing-application="" '
-    if delete_writing_library.lower() == "yes":
-        cmd_line = cmd_line + '--set muxing-application="" '
-    command = f'mkvpropedit {cmd_line}'
-    process = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    print('\n')
-    print(process.stdout.decode())
+        print('\nInput is invalid.\nTry again...')
+        exit()
 
-print("|")
-print("|--Do you want to save tags as a XML file?")
-save_xml = input("|--Type 'Yes' or 'No': ")
-if save_xml.lower() == "yes":
-    save_location = os.path.abspath(input("|--Enter folder path to save XML file: "))
-    os.chdir(save_location)    
-    with open(xml_name, 'wb') as xml_file:
-        xml_file.write(xml_data)
-    execute_propedit()
-else:
-    with open(xml_name, 'wb') as xml_file:
-        xml_file.write(xml_data)
-    execute_propedit()
-    os.remove(xml_name)
+# Generates a xml file with the tag data in the dictionary
+
+def generate_xml(inputFile, xmlPath):
+    get_tags(inputFile)
+    nodeTags = Element('Tags')
+    nodeTag = Element('Tag')
+    for name, value in tagsDict.items():
+        nodeSimple = Element('Simple')
+        nodeName = Element('Name')
+        nodeName.text = name
+        nodeSimple.append(nodeName)
+        nodeString = Element('String')
+        nodeString.text = value
+        nodeSimple.append(nodeString)
+        nodeTagLanguageIETF = Element('TagLanguageIETF')
+        nodeTagLanguageIETF.text = 'und'
+        nodeSimple.append(nodeTagLanguageIETF)
+        nodeTag.append(nodeSimple)
+    nodeTags.append(nodeTag)
+    xmlFile = tostring(nodeTags)
+    xmlFile = b'<?xml version="1.0" encoding="UTF-8"?>' + xmlFile
+
+    with open(xmlPath, 'wb') as xmlDoc:
+        xmlDoc.write(xmlFile)
+
+# process the xml file with mkvpropedit.
+
+def process_mkvpropedit(inputFile, xmlPath):
+    generate_xml(inputFile, xmlPath)
+
+    commandLine = f'"{os.path.abspath(inputFile)}" --tags all:"{xmlPath}" '
+    if Title.lower() == "delete":
+        commandLine = commandLine + f'--edit info --delete title '
+    elif Title:
+        commandLine = commandLine + f'--edit info --set title="{Title}" '
+    elif not Title:
+        commandLine = commandLine
+
+    encodedDate = input('\nDo you want to remove encoded date? [y/n] ')
+    writingApplication = input('Do you want to remove writing application? [y/n] ')
+    writingLibrary = input('Do you want to remove writing library? [y/n] ')
+
+    if (encodedDate == "y") or (encodedDate == "yes"):
+        commandLine = commandLine + f'--delete date '
+    if (writingApplication == "y") or (writingApplication == "yes"):
+        commandLine = commandLine + f'--set writing-application="" '
+    if (writingLibrary == "y") or (writingLibrary == "yes"):
+        commandLine = commandLine + f'--set muxing-application="" '
+
+    command = f'mkvpropedit {commandLine}'
+    process = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    print('\n' + process.stdout.decode())
+
+    saveXML = input(f'Do you want to keep "{xmlPath}"? [y/n] ')
+    if (saveXML.lower() == 'y') or (saveXML.lower() == 'yes'):
+        subprocess.run(f'xmlformat --overwrite {xmlPath}', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    else:
+        os.remove(xmlPath)
+
+def main():
+    if inputCount == 2:
+        if os.path.isfile(sys.argv[1]) is False:
+            for inputFile in os.listdir(sys.argv[1]):
+                if os.path.splitext(inputFile)[1] in ['.mkv', '.webm']:
+                    inputList.append(inputFile)
+            os.chdir(sys.argv[1])
+        else:
+            inputList.append(sys.argv[1])
+    elif inputCount > 2:
+        for inputID in range(1, inputCount):
+            inputList.append(sys.argv[inputID])
+    else:
+        print(f'Please provide inputs...')
+
+    for file in inputList:
+        sourceFolder = os.path.dirname(file)
+        sourceName = os.path.splitext(file)[0]
+        xml = os.path.join(sourceFolder, (f'{sourceName}_tags.xml'))
+        process_mkvpropedit(file, xml)
+
+if __name__ == "__main__":
+    main()
